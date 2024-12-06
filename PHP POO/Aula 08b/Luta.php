82796e536c9adf84652a2b4bf56cbd0fcdf74322
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-06 14:47:49 -0400

    index ok e terminei de colocar os metodos especiais get & set

---
+++ PHP POO/Aula 08b/Luta.php
@@ -0,0 +1,60 @@
+<?php 
+    require_once 'Lutador.php';
+    class Luta {
+        // Atributos
+        private $desafiado;
+        private $desafiante;
+        private $rounds;
+        private $aprovada;
+
+        // Métodos 
+        public function marcarLuta() {
+
+        }
+        public function lutar() {
+            if ($this->aprovada) {
+                $this->desafiado.$this->apresentar()
+            }
+        }
+        public function marcarLuta($l1, $l2) {
+            if ($l1.$this->getCategoria() = $l2.$this->getCategoria()) and ($l1 <> $l2) {
+                $this->aprovada = true;
+                $this->desafiado = $l1;
+                $this->desafiante = $l2;
+            } else {
+                $this->aprovada = false;
+                $this->desafiado = null;
+                $this->desafiante = null;
+            }
+        }
+
+        // Métodos Especiais
+        public function getDesafiado() {
+            return $this->desafiado;
+        }
+        public function setDesafiado($dd) {
+            $this->desafiado = $dd;
+        }
+        
+        public function getDesafiante() {
+            return $this->desafiante;
+        }
+        public function setDesafiante($de) {
+            $this->desafiante = $de;
+        }
+
+        public function getRounds() {
+            return $this->rounds;
+        }
+        public function setRounds($ro) {
+            $this->rounds = $ro;
+        }
+
+        public function getAprovada() {
+            return $this->aprovada;
+        }
+        public function setAprovada($ap) {
+            $this->aprovada = $ap;
+        }
+    }
+?>
