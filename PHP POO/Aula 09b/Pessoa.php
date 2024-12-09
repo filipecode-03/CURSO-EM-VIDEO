777b94f7408b18acd648f0428773edb8031e5e11
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-09 09:06:10 -0400

    metodos especiais de Pessoas.php ok

--- PHP POO/Aula 09b/Pessoa.php
+++ PHP POO/Aula 09b/Pessoa.php
@@ -7,7 +7,36 @@
 
         // Métodos
         public function fazerAniver() {
-            
+            $this->idade ++;
+        }
+
+        // Métodos Especiais
+        function __construct($nome, $idade, $sexo)
+        {
+            $this->nome = $nome;
+            $this->idade = $idade;
+            $this->sexo = $sexo;
+        }
+
+        function getNome() {
+            return $this->nome;
+        }
+        function setNome($no) {
+            $this->nome = $no;
+        }
+
+        function getIdade() {
+            return $this->idade;
+        }
+        function setIdade($id) {
+            $this->idade = $id;
+        }
+
+        function getSexo() {
+            return $this->sexo;
+        }
+        function setSexo($se) {
+            $this->sexo = $se;
         }
     }
 ?>
