778b326dae1c120aadc07921f3f4876473e72d78
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-10 14:25:46 -0400

    Aula 12a ok

---
+++ PHP POO/Aula 12a/assinaturaDoMetodo.php
@@ -0,0 +1,19 @@
+<?php /*  
+    class assina {
+        public function calcMedia($n1, $n2) {
+
+        }
+        public function calcMedia($v1, $v2) {
+
+        }
+        public function calcMedia($bim, $n1, $n2) {
+
+        }
+        public function calcMedia($n1, $n2, $n3, $n4) {
+
+        }
+        public function calcMedia($medMin, $medMax, $sit, $bim) {
+
+        }
+    }
+?> */
