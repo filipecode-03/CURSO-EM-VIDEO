6d58753b403c226af9434b243f7a6351833640fd
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-12 08:51:38 -0400

    video ok

--- PHP POO/Aula 14b/Video.php
+++ PHP POO/Aula 14b/Video.php
@@ -1,10 +1,66 @@
 <?php 
-    class Video {
+    class Video implements AcoesVideo {
         // Atributos
         private $titulo;
         private $avaliacao;
         private $views;
         private $curtidas;
         private $reproduzindo;
+
+        // Métodos
+        public function play() {
+
+        }
+        public function pause() {
+
+        }
+        public function like() {
+
+        }
+
+        // Métodos Especiais
+        public function __construct($titulo)
+        {
+            $this->titulo = $titulo;
+            $this->avaliacao = 1;
+            $this->views = 0;
+            $this->curtidas = 0;
+            $this->reproduzindo = false;
+        }
+        public function getTitulo() {
+            return $this->titulo;
+        }
+        public function setTitulo($ti) {
+            $this->titulo = $ti;
+        }
+
+        public function getAvaliacao() {
+            return $this->avaliacao;
+        }
+        public function setAvaliacao($av) {
+            $this->avaliacao = $av;
+        }
+
+        public function getViews() {
+            return $this->views;
+        }
+        public function setViews($vi) {
+            $this->views = $vi;
+        }
+
+        public function getCurtidas() {
+            return $this->curtidas;
+        }
+        public function setCurtidas($cu) {
+            $this->curtidas = $cu;
+        }
+
+        public function getReproduzindo() {
+            return $this->reproduzindo;
+        }
+        public function setReproduzindo($re) {
+            $this->reproduzindo = $re;
+        }
+
     }
 ?>
