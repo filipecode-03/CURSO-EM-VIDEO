067899a87d0c75ca56cae04469284521a43c5e1e
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-12 09:17:26 -0400

    finalizado

--- PHP POO/Aula 14b/Gafanhoto.php
+++ PHP POO/Aula 14b/Gafanhoto.php
@@ -1,12 +1,35 @@
 <?php 
-    class Gafanhoto {
+    require_once 'Pessoa.php';
+    class Gafanhoto extends Pessoa {
         // Atributos
         private $login;
         private $totAssistido;
 
         // Métodos
-        public function viuMaisUm() {
-            
+        public function assistirMaisUm() {
+            $this->totAssistido ++;
+        }
+
+        // Métodos Especiais
+        public function __construct($nome, $idade, $sexo, $login)
+        {
+            parent::__construct($nome, $idade, $sexo);
+            $this->login = $login;
+            $this->totAssistido = 0;
+        }
+
+        public function getLogin() {
+            return $this->login;
+        }
+        public function setLogin($lo) {
+            $this->login = $lo;
+        }
+
+        public function getTotAssistido() {
+            return $this->totAssistido;
+        }
+        public function setTotAssistindo($ta) {
+            $this->totAssistido = $ta;
         }
     }
 ?>
--- PHP POO/Aula 14b/index.php
+++ PHP POO/Aula 14b/index.php
@@ -8,9 +8,13 @@
 <body>
     <?php 
         require_once 'Video.php';
+        require_once 'Gafanhoto.php';
         $v[0] = new Video("Aula 1 de POO");
         $v[1] = new Video("Aula 12 de PHP");
         $v[2] = new Video("Aula 15 de HTML5");
+
+        $g[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
+        $g[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
     ?>
 </body>
 </html>
