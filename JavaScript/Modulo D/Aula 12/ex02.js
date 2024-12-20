b1bab70dfa8f129662495cffb6625e8821a747bf
filipecode-03 <158044004+filipecode-03@users.noreply.github.com>
2024-12-20 11:45:36 -0400

    Aula 12 do curso de JavaScript finalizada

--- JavaScript/Modulo D/Aula 12/ex02.js
+++ JavaScript/Modulo D/Aula 12/ex02.js
@@ -1,2 +1,10 @@
-var hora = 8
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora}`)
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
